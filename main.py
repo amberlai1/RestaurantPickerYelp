38b0fa38846bda682dfd1e74c6d58614905393c3
amberlai1 <alai8@u.rochester.edu>
2022-08-03 07:11:30 -0400

    initial GUI

--- main.py
+++ main.py
@@ -1,20 +1,56 @@
 import requests
 import pandas as pd
-# from PyQt5.QtWidgets import QApplication, QWidget
-# import sys
-# # gui
-# app = QApplication(sys.argv)
-# window = QWidget()
-# window.show()
-# #execute app
-# app.exec()
+import tkinter as tk
+from tkinter import ttk
+
+
+#gui 
+root = tk.Tk()
+root.title("Restaurant Picker")
+message = tk.Label(root, text="Welcome to the restaurant picker ")
+message.pack()
+
+
+#dimensions
+window_width = 500
+window_height = 500
+
+# get the screen dimension
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+# find the center point
+center_x = int(screen_width/2 - window_width / 2)
+center_y = int(screen_height/2 - window_height / 2)
+
+# set the position of the window to the center of the screen
+root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+
+# button 
+def button_clicked():
+    print('Button clicked')
+button = ttk.Button(root, text='Click Me', command=button_clicked)
+button.pack()
+
+
+# making everything clear
+try:
+    from ctypes import windll
+
+    windll.shcore.SetProcessDpiAwareness(1)
+finally:
+    root.mainloop()
+
+#executing loop 
+root.mainloop()
+
 
 # defining variables
 clientId = "NJIVdjU7-8vVZuVvkdGdBg"
 apiKey = "cxt68BFcIE0oso3ZtPSSySd3PO0RuzGpdPUM5KkwCxaFaCHTbrU2tiD_bJ1ZfS-OVU2b8NM9Sokd5nrSPLDMqRp4iug9gojYQDgH4cqpyljodcqq2yoKVtfepGrMYnYx"
 url = 'https://api.yelp.com/v3/businesses/search'
 
-print("Welcomes to the Restaurant Picker! Answer a couple questions below to find the perfect restaurant for you!")
+print("Welcome to the Restaurant Picker! Answer a couple questions below to find the perfect restaurant for you!")
 
 #search criteria
 term = input("What type of cuisine do you want ")
@@ -38,25 +74,30 @@ response = requests.get(url, headers=headers, params=url_params)
 print(response.text)
 
 #making a dataframe
+newdict = {}
 df = pd.DataFrame.from_dict(response.json()['businesses'])
-dataframeprice = df.loc[['price'] <= int(pricerange)]
+dataframeprice = df.loc[df['price'] <= pricerange]
 df1 = dataframeprice[['name', 'url', 'price', 'location']].copy()
-print(len(df)) #Print how many rows
+print(len(df1)) #Print how many rows
 print(df1.columns) #Print column names
 
-#df.head()
 df2 = df1.sample()
+newdict[df2] 
 print(df2)
 
 # make a reroll method
-
 def decisionreroll():
     reroll = input("Would you like to pick a new restaurant? ")
     while reroll != "no":
-        df3 = df1.sample()
-        print(df3)
+        if df3 != newdict:
+            df3 = df1.sample()
+            newdict[df3]
+            print(df3)
+        else:
+            df3 = df1.sample()
         reroll = input("Would you like to pick a new restaurant? ")
 
-    print("Hope you enjoy your restaurant!") #, df3[['name']])
+    return "Hope you enjoy your restaurant!" # , df3[['name']]
 
 print(decisionreroll())
+
